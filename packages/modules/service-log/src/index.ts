911d8db1b53488027e15653949c9747f734a4fa7
Mahdi Karami <mahdi.karami@digikala.com>
2026-05-14 22:15:48 +0330

    feat: Some minor changes

--- packages/modules/service-log/src/index.ts
+++ packages/modules/service-log/src/index.ts
@@ -1,14 +1,12 @@
 import { Module } from "@medusajs/framework/utils";
 
 import ServiceLogModuleService from "./service";
-import { Migration20250514000001 } from "./migrations/Migration20250514000001";
 
 export const SERVICE_LOG_MODULE = "service_log";
 export { ServiceLogModuleService };
 
 export default Module(SERVICE_LOG_MODULE, {
   service: ServiceLogModuleService,
-  migrations: [Migration20250514000001],
 });
 
 
